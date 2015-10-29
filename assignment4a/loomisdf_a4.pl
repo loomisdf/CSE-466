d04c62546af1ee3e4a2d34cfb132aaadb49ea230
loomisdf <loomisdf@cec-ben002-d1.csi.miamioh.edu>
2015-10-29 12:39:39 -0400

    use Bio::seqIO to parse gene sequence file and print the selected sequence

---
+++ assignment4a/loomisdf_a4.pl
@@ -0,0 +1,89 @@
+#! usr/bin/perl -w
+use strict;
+use Bio::SeqIO;
+
+my %sequences;
+my $file = shift;
+
+# use SeqIO to extract gene sequences
+my $stream = Bio::SeqIO->new(-file => $file,
+                             -format => 'fasta');
+
+while (my $seq_objt=$stream->next_seq()) {
+    my $seq_name=$seq_objt->display_id();
+    my $seq_desc=$seq_objt->desc();
+    my $seq_base=$seq_objt->seq();
+	my $key = $seq_name." ".$seq_desc;
+	$sequences{$key} = $seq_base;
+}
+
+my $seqname = <STDIN>;
+chomp($seqname);
+
+printWithSpacer($sequences{$seqname});
+
+# Print out the sequence, nice and formatted
+sub printWithSpacer {
+	my $seq = shift;;
+	my $space = " ";
+	# Figure out the number of lines to print
+	my $lineNum = int(length($seq) / 100);
+
+	# Check if there needs to be an extra line
+	if( length($seq) % 100 != 0) {
+		$lineNum++;
+	}
+
+	# Get the number of digits
+	my $digits = length("".$lineNum);
+
+	# Figure out padding for line and necleotide position
+	my $linePad = " ";
+
+	# Begin printing the sequence
+	print("    ".$linePad.
+	      "         1".$space.
+		  "         2".$space.
+		  "         3".$space.
+		  "         4".$space.
+		  "         5".$space.
+		  "         6".$space.
+		  "         7".$space.
+		  "         8".$space.
+		  "         9".$space.
+		  "        10\n");
+
+	print("Line".$linePad.
+		  "1234567890".$space.
+		  "1234567890".$space.
+		  "1234567890".$space.
+		  "1234567890".$space.
+		  "1234567890".$space.
+		  "1234567890".$space.
+		  "1234567890".$space.
+		  "1234567890".$space.
+		  "1234567890".$space.
+		  "1234567890\n");
+
+	my $indent = " ";
+	for(my $i = 1; $i <= $lineNum; $i++) {
+		my $padding = "";
+		if($i < 10) {
+			$padding .= "  ";
+		} elsif ($i >= 10 && $i < 100) {
+			$padding .= " ";
+		}
+		print($indent.$padding."$i ");
+
+		# $k starting point is 100 times the current line number - 1.
+		# The subtraction ensures that $k starts at 0.
+		# Loop goes until the next 100 characters have been processed
+		for(my $k = ($i - 1) * 100; $k < 100 * $i; $k+=10) {
+			if($k > length($seq)) {
+				last;
+			}
+			print(substr($seq, $k, 10).$space);
+		}
+		print("\n");
+	}
+}
